79496433c63283b9580c1d5889a4bf68bec5195e
sharadkeshari <sharad.keshari399@gmail.com>
2022-04-12 10:30:52 +0530

    Repository library setup and dependency injection

---
+++ BookRepository/IBookRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace BookRepository
+{
+    public interface IBookRepository
+    {
+    }
+}
--- BookStore.API/Startup.cs
+++ BookStore.API/Startup.cs
@@ -1,9 +1,11 @@
+using BookRepository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
@@ -32,6 +34,7 @@ namespace BookStore.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore.API", Version = "v1" });
             });
+            services.TryAddTransient<IBookRepository, BookRepository.BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
