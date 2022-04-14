c182b355bd853ce2a237abb4ca446ab59e4c4ade
sharadkeshari <sharad.keshari399@gmail.com>
2022-04-14 20:02:36 +0530

    ModelValidation Implementation

--- BookRepository/Models/BookModel.cs
+++ BookRepository/Models/BookModel.cs
@@ -1,8 +1,11 @@
-﻿namespace BookRepository.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookRepository.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="The title is required. Please add that.")]
         public string Title { get; set; }
         public string Description { get; set; }
     }
